0d2b36d2571a9f527cd88e69564e6c6ca93340e6
Oscar Gomez <osckar96@gmail.com>
2024-05-24 18:12:21 +0200

    feat(react-hexagonal-learn): improve the public screen view

--- src/infraestructure/ui/public/public.tsx
+++ src/infraestructure/ui/public/public.tsx
@@ -1,40 +1,28 @@
+import './public.css';
+
 function Public() {
     return (
         <>
-            <table>
-                <tr>
-                    <td valign="middle" className="ticket-actual">
-                        <span id="lblTicket1" className="ticket-actual-numero">Ticket W</span>
-                        <br/>
-                        <span id="lblEscritorio1" className="ticket-actual-escritorio">Escritorio W</span>
-                    </td>
-                    <td>
-                        <table>
-                            <tr>
-                                <td>
-                                    <span id="lblTicket2" className="ticket-secundario">Ticket X</span>
-                                    <br/>
-                                    <span id="lblEscritorio2">Escritorio X</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span id="lblTicket3" className="ticket-secundario">Ticket Y</span>
-                                    <br/>
-                                    <span id="lblEscritorio3">Escritorio Y</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span id="lblTicket4" className="ticket-secundario">Ticket Z</span>
-                                    <br/>
-                                    <span id="lblEscritorio4">Escritorio Z</span>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
+            <div className="table-tickets">
+                <div className="current-ticket">
+                    <span id="lblTicket1" className="ticket-actual-numero">Ticket W</span>
+                    <span id="lblEscritorio1" className="ticket-actual-escritorio">Escritorio W</span>
+                </div>
+                <div className="next-tickets">
+                    <div>
+                        <span id="lblTicket2" className="ticket-secundario">Ticket X</span>
+                        <span id="lblEscritorio2">Escritorio X</span>
+                    </div>
+                    <div>
+                        <span id="lblTicket3" className="ticket-secundario">Ticket Y</span>
+                        <span id="lblEscritorio3">Escritorio Y</span>
+                    </div>
+                    <div>
+                        <span id="lblTicket4" className="ticket-secundario">Ticket Z</span>
+                        <span id="lblEscritorio4">Escritorio Z</span>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
