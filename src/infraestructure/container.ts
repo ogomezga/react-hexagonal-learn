30d22dde0d91f8fa7e8c4e8f1e0a1b671f261b84
Oscar Gomez <osckar96@gmail.com>
2024-05-26 13:37:01 +0200

    feat(react-hexagonal-learn): create an awilix container to register the uc

---
+++ src/infraestructure/container.ts
@@ -0,0 +1,26 @@
+import {
+    AwilixContainer,
+    createContainer,
+    asFunction,
+} from 'awilix';
+import useCreateNewTicket from '../useCases/useCreateNewTicket.ts';
+
+
+let container: AwilixContainer;
+
+export function getMainContainer() {
+    if (!container) {
+        container = createContainer();
+    }
+
+    container.register({
+        /**
+         * Given that Awilix runs as soon as the server starts to register dependencies, the useEffect is executed at that moment.
+         * So, when it's required in the component, it doesn't perform the necessary actions because it doesn't detect that the component is "mounting" or updating.
+         * That's why it should be registered as an implementation of a function.
+         */
+        useCreateNewTicket: asFunction(() => useCreateNewTicket).singleton(),
+    });
+
+    return container;
+}
