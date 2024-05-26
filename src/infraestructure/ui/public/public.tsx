e827e95873b6d2ecd3c692fb2327049080b3ef10
Oscar Gomez <osckar96@gmail.com>
2024-05-26 16:28:06 +0200

    feat(react-hexagonal-learn): implement the custom hook

--- src/infraestructure/ui/public/public.tsx
+++ src/infraestructure/ui/public/public.tsx
@@ -1,25 +1,35 @@
 import './public.css';
+import {useDependencies} from '../../dependency-injection.tsx';
 
 function Public() {
+    const { useDisplayTickets } = useDependencies().cradle;
+    const { lblTicket1,
+        lblTicket2,
+        lblTicket3,
+        lblTicket4,
+        lblDesktop1,
+        lblDesktop2,
+        lblDesktop3,
+        lblDesktop4} = useDisplayTickets();
     return (
         <>
             <div className="table-tickets">
                 <div className="current-ticket">
-                    <span id="lblTicket1" className="ticket-actual-numero">Ticket W</span>
-                    <span id="lblEscritorio1" className="ticket-actual-escritorio">Escritorio W</span>
+                    <span id="lblTicket1">{ lblTicket1 }</span>
+                    <span id="lblDesktop1">{ lblDesktop1 }</span>
                 </div>
                 <div className="next-tickets">
                     <div>
-                        <span id="lblTicket2" className="ticket-secundario">Ticket X</span>
-                        <span id="lblEscritorio2">Escritorio X</span>
+                        <span id="lblTicket2">{ lblTicket2 }</span>
+                        <span id="lblDesktop2">{ lblDesktop2 }</span>
                     </div>
                     <div>
-                        <span id="lblTicket3" className="ticket-secundario">Ticket Y</span>
-                        <span id="lblEscritorio3">Escritorio Y</span>
+                        <span id="lblTicket3">{ lblTicket3 }</span>
+                        <span id="lblDesktop3">{ lblDesktop3 }</span>
                     </div>
                     <div>
-                        <span id="lblTicket4" className="ticket-secundario">Ticket Z</span>
-                        <span id="lblEscritorio4">Escritorio Z</span>
+                        <span id="lblTicket4">{ lblTicket4 }</span>
+                        <span id="lblDesktop4">{ lblDesktop4 }</span>
                     </div>
                 </div>
             </div>
